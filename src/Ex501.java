9add39dcc2044fb825a4edbac4dd25fe73d8fba3
kkl0104908 <162276829+kkl0104908@users.noreply.github.com>
2024-03-26 15:59:18 +0900

    4주차 끝~!

---
+++ src/Ex501.java
@@ -0,0 +1,17 @@
+
+public class Ex501 {
+	
+	public static void hello(String name) {
+		System.out.println("Hello, "+ name);
+	}
+	
+	public static void main(String[] args) {
+		hello("Aaron");
+		hello("Tom");
+		hello("Loki");
+		hello("Rocky");
+		hello("Aaron");
+		
+	}
+
+}
